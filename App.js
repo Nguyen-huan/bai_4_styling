9afd2c23faef4730ecee14b4265fb51d61980de9
Huân Nguyễn <huan06949@gmail.com>
2022-09-08 21:19:07 +0700

    bai_4_styling

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.square_1}>Square 1</Text>
+      <Text style={styles.square_2}>Square 2</Text>
+      <Text style={styles.square_3}>Square 3</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+  },
+  square_1: {
+    height: 80,
+    width: 80,
+    textAlign: 'center',
+    lineHeight: 80,
+    backgroundColor: '#7ce0f9',
+    color: 'black',
+  },
+  square_2: {
+    height: 80,
+    width: 80,
+    textAlign: 'center',
+    lineHeight: 80,
+    backgroundColor: '#4cc2c2',
+    color: 'black',
+  },
+  square_3: {
+    height: 80,
+    width: 80,
+    textAlign: 'center',
+    lineHeight: 80,
+    backgroundColor: '#ff637c',
+    color: 'black',
   },
 });
+
+
